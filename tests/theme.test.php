1a25822b1fe211de431671849f16826f8a273e39
Raf <7909000@gmail.com>
2012-10-20 18:55:37 +0500

    added new test methods
    Signed-off-by: Raf <7909000@gmail.com>

--- tests/theme.test.php
+++ tests/theme.test.php
@@ -3,29 +3,42 @@
 class TestTheme extends PHPUnit_Framework_TestCase
 {
 	public $theme;
+	public $themeName = "DefaultTheme";
 
 	public function setup()
 	{
 		Bundle::start('theme');
-	}
-
-	public function testThemeInstance()
-	{
-		
 		$config = array(
             'theme_path'=>'themes',  
         );
     	
-    	$this->theme = new Theme("admin",$config);
+    	$this->theme = new Theme($this->themeName,$config);
+	}
 
+	public function testThemeInstance()
+	{
 		return $this->assertInstanceOf('Theme', $this->theme);
 	}
 
 
-	public function testThemeAttributes()
+	public function testThemeAttributeThemeName()
 	{
-
+		return $this->assertObjectHasAttribute('_theme_name', $this->theme);
 	}
+
+	public function testThemeAttributeThemeNameReturnString()
+	{
+		$theme_name = $this->theme->_theme_name;
+		return $this->asserttrue(is_string($theme_name));
+	}
+
+	public function testThemeSetTheme()
+	{
+		$this->theme->set_theme('NewTheme');
+		return $this->assertTrue(($this->theme->_theme_name == 'NewTheme'), 'set_theme method not passing the new theme');
+	}
+
+	
 
 
 }
