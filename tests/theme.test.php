effccce1cabc600892c6eea8880d9546742c0bd7
Raf <7909000@gmail.com>
2012-10-20 18:34:36 +0500

    Revert "added test methods to test class"
    This reverts commit e8927b8255150a14c5f6c30c41afcc962710e705.
    
    Signed-off-by: Raf <7909000@gmail.com>

--- tests/theme.test.php
+++ tests/theme.test.php
@@ -2,30 +2,9 @@
 
 class TestTheme extends PHPUnit_Framework_TestCase
 {
-	public $theme;
 
-	public function setup()
-	{
+	public function testThemeInstance(){
 		Bundle::start('theme');
+		return $this->assertInstanceOf('Theme', new Theme);
 	}
-
-	public function testThemeInstance()
-	{
-		
-		$config = array(
-            'theme_path'=>'themes',  
-        );
-    	
-    	$this->theme = new Theme("admin",$config);
-
-		return $this->assertInstanceOf('Theme', $this->theme);
-	}
-
-
-	public function testThemeAttributes()
-	{
-
-	}
-
-
 }
